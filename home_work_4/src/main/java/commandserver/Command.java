37fee3f8a98c0576c6af2a4fb83f17558d2b0928
wolfchkov <wolfchkovaj@gmail.com>
2018-03-20 21:58:39 +0200

    Update Command.java
    Замечания

--- home_work_4/src/main/java/commandserver/Command.java
+++ home_work_4/src/main/java/commandserver/Command.java
@@ -4,5 +4,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public interface Command {
+    /**
+    Этот метод должен принимать кроме PrintWriter еще и список аргументов, к примеру String ... args
+    либо массив что впринципе одно и тоже
+    Тогда команды можно создать один раз в конструкторе CommandMaper'a и не каждый раз во время парсинга
+    **/ 
     void execute(PrintWriter writer) throws IOException, IllegalAccessException;
 }
