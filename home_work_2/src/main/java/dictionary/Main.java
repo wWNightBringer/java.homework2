b84dca3270d7976b437e1bd509c719d8531ecd22
wolfchkov <wolfchkovaj@gmail.com>
2018-03-20 21:39:37 +0200

    Update Main.java
    Замечания

--- home_work_2/src/main/java/dictionary/Main.java
+++ home_work_2/src/main/java/dictionary/Main.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class Main {
     private static ReadFile readFile;
 
+    /**
+    Программа должна быть User-Frendly
+    А тут получается мне нужно для каждого слова ее запускать 
+    Пользователи такой пользоваться не будут :)
+    Где цикл, с вопросом что перевести или выйти из программы?
+    **/
     public static void main(String[] args) throws IOException {
         Scanner scanner=new Scanner(System.in);
         String line;
