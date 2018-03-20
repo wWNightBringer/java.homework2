c78e10dc28c03da1c96911cae751fb7ec4d241f6
wolfchkov <wolfchkovaj@gmail.com>
2018-03-20 21:44:35 +0200

    Update ReadFile.java
    Замечания

--- home_work_2/src/main/java/dictionary/ReadFile.java
+++ home_work_2/src/main/java/dictionary/ReadFile.java
@@ -5,10 +5,25 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Stream;
 
+/**
+ReadFile очень плохое название для класса
+Очень общее, несет в себе глагол.
+DictionaryReader вот это к примеру лучше, потому что класс - это имя существительное
+Методы - это глаголы.
+
+**/
 public class ReadFile {
+   
     private Properties properties;
+    
+    /**
+    Почему Map без Generic?
+    **/
     public Map readFromFile() throws IOException {
+        //Не отформатированно 
         properties=new Properties();
+        //Имя файла должно быть хотя бы константой как статическое поле класса!
+        //И нужно использовать Files, File - это устаревший класс
         File file=new File("C:\\Users\\Nightbringer\\IdeaProjects\\HomeWork2\\src\\main\\java\\alfabet\\File\\Language.txt");
      BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
      String string;
